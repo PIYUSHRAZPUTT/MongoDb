36c2186c65e411f00ff5fea55eb1bfb2390fe760
PIYUSHRAZPUTT <piyushrajpoot1488999@gmail.com>
2026-06-04 23:05:31 +0530

    complete notes api for CRUD operation in database

--- database2/src/app.js
+++ database2/src/app.js
@@ -1,5 +1,50 @@
-import express from 'express';
+import express from "express";
+import Note from "./models/note.model.js";
 
-const app= express();
+const app = express();
+
+app.use(express.json());
+
+// Post the notes
+app.post("/notes", async (req, res) => {
+  const data = req.body;
+  await Note.create({
+    title: data.title,
+    description: data.description,
+    createdAt: data.createdAt,
+  });
+  res.status(201).json({
+    message: "notes created successfully",
+  });
+});
+
+// Get the notes
+app.get("/notes", async (req, res) => {
+  const notes = await Note.find();
+
+  res.status(200).json({
+    message: "notes fetched  successfully",
+    notes: notes,
+  });
+});
+
+// delete the notes
+app.delete("/notes/:id", async (req, res) => {
+  const id = req.params.id;
+  await Note.findByIdAndDelete(id);
+  res.status(200).json({
+    message: "notes deleted  successfully",
+  });
+});
+
+// updates the notes
+app.patch("/notes/:id", async (req, res) => {
+  const id = req.params.id;
+  const description = req.body.description;
+  await Note.findByIdAndUpdate(id, { description: description });
+  res.status(200).json({
+    message: "notes updated successfully",
+  });
+});
 
 export default app;
---
+++ database2/src/models/note.model.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const noteSchema= new mongoose.Schema({
+    title:{type:String, required:true},
+    description:{type:String, required:true},
+    cretedAt:{type:Date, default:new Date}
+});
+
+const Note= mongoose.model('Note', noteSchema);
+
+export default Note;
